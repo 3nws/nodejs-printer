a5ffccaf3fd077865139c070262dd985940bf0f4
Enes Kurbetoğlu <eneskurbetoglu1@gmail.com>
2025-06-02 13:34:32 +0300

    add set default printer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as print } from "./print/print";
 export { default as getPrinters } from "./get-printers/get-printers";
 export { default as getDefaultPrinter } from "./get-default-printer/get-default-printer";
+export { default as setDefaultPrinter } from "./set-default-printer/set-default-printer";
 export { PrintOptions } from "./print/print";
 
 export type Printer = {
---
+++ src/set-default-printer/set-default-printer.ts
@@ -0,0 +1,31 @@
+import execFileAsync from "../utils/exec-file-async";
+import throwIfUnsupportedOperatingSystem from "../utils/throw-if-unsupported-os";
+import isValidPrinter from "../utils/windows-printer-valid";
+import { Printer } from "..";
+
+async function setDefaultPrinter(name: string): Promise<Printer | null> {
+  try {
+    throwIfUnsupportedOperatingSystem();
+
+    const { stdout } = await execFileAsync("Powershell.exe", [
+      "-Command",
+      `Get-CimInstance Win32_Printer -Filter "Name='${name}'" Invoke-CimMethod -InputObject $printer -MethodName SetDefaultPrinter`,
+    ]);
+
+    const printer = stdout.trim();
+
+    // If stdout is empty, there is no default printer
+    if (!stdout) return null;
+
+    const { isValid, printerData } = isValidPrinter(printer);
+
+    // DeviceID or Name not found
+    if (!isValid) return null;
+
+    return printerData;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default setDefaultPrinter;
